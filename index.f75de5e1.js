7e0b02df8bc32c7ae92af650c2a5168efd004b52
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-01-15 20:53:34 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const inputElements = document.getElementsByTagName("input");
+function getCorrectStr(str) {
+    const newStr = str.split(/(?=[A-Z])/).join(" ");
+    return newStr;
+}
+[
+    ...inputElements
+].forEach((input)=>{
+    const { name: str } = input;
+    const validStr = getCorrectStr(str);
+    input.placeholder = validStr.charAt(0).toUpperCase() + validStr.slice(1);
+    const label = document.createElement("label");
+    label.className = "field-label";
+    label.htmlFor = input.id;
+    label.textContent = validStr.toUpperCase();
+    input.parentElement.append(label);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
